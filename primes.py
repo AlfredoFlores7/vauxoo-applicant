6605ae7dab2ef77fd5cc396a1ccec8f698641e44
AlfredoFlores7 <alfredo.flores.v64hotmail.com>
2015-05-11 13:52:16 -0500

    Modificacion de clase primes

--- primes.py
+++ primes.py
@@ -8,9 +8,25 @@ class PrimeClass(object):
     Your class documentation here
     """
 
-    def is_prime(self, num_int):
+num= int(raw_input("Ingresa u numero")
+
+	for i in range (0, num):
+	if isPrime(i):
+	print i
+
+
+    def is_prime(num):
         """
         Your method documentation here
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num < 2:    
+           return False
+        for i in range(2, num):
+	    if num % i == 0:   
+	    return False
+         return True 
+      print es_primo(3)
+
+
+
